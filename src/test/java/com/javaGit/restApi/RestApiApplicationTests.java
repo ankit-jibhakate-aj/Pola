74917c6daaa7f7680661956836d524b2d9930c4c
ankit jibhakate <ankitubj@gmail.com>
2022-08-27 08:56:00 +0530

    add changes on code

---
+++ src/test/java/com/javaGit/restApi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaGit.restApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
